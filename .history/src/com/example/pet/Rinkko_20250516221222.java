7bc0f64f878d593a640c1a64e8826cf9f68e285c
nanakumo <manka.c2022@proton.me>
2025-05-16 23:48:29 +0900

    喵喵世界

---
+++ .history/src/com/example/pet/Rinkko_20250516221222.java
@@ -0,0 +1,12 @@
+package com.example.pet;
+
+public class Rinkko {
+
+    private String nma
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ .history/src/com/example/pet/Rinkko_20250516221225.java
@@ -0,0 +1,13 @@
+package com.example.pet;
+
+public class Rinkko {
+
+    private String name;
+    
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
