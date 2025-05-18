3d15931876437c59b7e45765c0992fb9380521b2
nanakumo <manka.c2022@proton.me>
2025-05-18 22:16:06 +0900

    第4题

---
+++ .history/src/com/example/pet/DebuffType_20250518204735.java
@@ -0,0 +1,10 @@
+package com.example.pet;
+
+public class DebuffType {
+    NONE("无"),
+    HUNGRY("饿了"),
+    THIRSTY("渴了"),
+    UNHAPPY("不开心"),
+    SICK("生病");
+
+}
---
+++ .history/src/com/example/pet/DebuffType_20250518205105.java
@@ -0,0 +1,14 @@
+package com.example.pet;
+
+public enum DebuffType {
+    NONE("无"),
+    HUNGRY("饿了"),
+    THIRSTY("渴了"),
+    UNHAPPY("不开心"),
+    SICK("生病");
+
+    private final 
+
+
+
+}
